d8d8fa0091c18afd1e2bf0243f9edfc3efef459c
Рустам <gayanov6063@gmail.com>
2020-02-18 15:50:55 +0300

    10 готово

--- lesson_002/10_store.py
+++ lesson_002/10_store.py
@@ -52,7 +52,42 @@ print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, '�
 # WARNING для знающих циклы: БЕЗ циклов. Да, с переменными; да, неэффективно; да, копипаста.
 # Это задание на ручное вычисление - что бы потом понять как работают циклы и насколько с ними проще жить.
 
-# TODO здесь ваш код
+table_code = goods['Стол']
+table_item = store[table_code][0]
+table_item1 = store[table_code][1]
+table_quantity = table_item['quantity']
+table_quantity1 = table_item1['quantity']
+table_total=table_quantity+table_quantity1
+table_price = table_item['price']
+table_price1 = table_item1['price']
+table_cost = (table_quantity * table_price)+(table_quantity1*table_price1)
+print('Стол -', table_total, 'шт, стоимость', table_cost, 'руб')
+
+sofa_code = goods['Диван']
+sofa_item = store[sofa_code][0]
+sofa_item1 = store[sofa_code][1]
+sofa_quantity = sofa_item['quantity']
+sofa_quantity1 = sofa_item1['quantity']
+sofa_total=sofa_quantity+sofa_quantity1
+sofa_price = sofa_item['price']
+sofa_price1 = sofa_item1['price']
+sofa_cost = (sofa_quantity * sofa_price)+(sofa_quantity1*sofa_price1)
+print('Диван -', sofa_total, 'шт, стоимость', sofa_cost, 'руб')
+
+chair_code = goods['Стул']
+chair_item = store[chair_code][0]
+chair_item1=store[chair_code][1]
+chair_item2=store[chair_code][2]
+chair_quantity = chair_item['quantity']
+chair_quantity1 = chair_item1['quantity']
+chair_quantity2 = chair_item2['quantity']
+chair_total=chair_quantity1+chair_quantity+chair_quantity2
+chair_price = chair_item['price']
+chair_price1=chair_item1['price']
+chair_price2=chair_item2['price']
+chair_cost = (chair_quantity*chair_price)+(chair_quantity1*chair_price1)+(chair_quantity2*chair_price2)
+print('Стул -', chair_total, 'шт, стоимость', chair_cost, 'руб')
+
 
 ##########################################################################################
 # ВНИМАНИЕ! После того как __ВСЯ__ домашняя работа сделана и запушена на сервер,         #
