b8ea059dab43f329930177ef507207f872b7dbb2
Рустам <gayanov6063@gmail.com>
2020-03-08 23:01:10 +0300

    00 Помогите пожалуйста разобраться

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
+from random import choice
 
 import simple_draw as sd
+from simple_draw import Point
 
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+point = sd.get_point(300, 300)
+radius_bubble = 50
+for _ in range(3):
+    radius_bubble += 5
+    sd.circle(center_position=point, radius=radius_bubble, width=2)
 
 # Написать функцию рисования пузырька, принммающую 3 (или более) параметра: точка рисования, шаг и цвет
-# TODO здесь ваш код
+def bubble(point, step, color_bubble):
+    radius_bubble = 50
+    for _ in range(3):
+        radius_bubble += step
+        sd.circle(center_position=point, radius=radius_bubble, width=2, color=color_bubble)
+
+
+bubble(point=sd.get_point(300, 300), radius_bubble=50, color_bubble=sd.random_color())
+#TODO Вот здесь даёт ошибку, получается что, мы указываем точку то есть point, после указываем
+#TODO радиус то есть radius_bubble, и цвет который будет выдаваться рандомно?
+
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
