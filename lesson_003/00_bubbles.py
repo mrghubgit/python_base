85d4dca71bc59eef7c643d1ab0389dec5ffb40e4
Рустам <gayanov6063@gmail.com>
2020-03-23 11:07:33 +0300

    Сделал пока три задания, в задании 02  как будто всё слишком просто

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 from random import choice
 
 import simple_draw as sd
@@ -21,19 +22,24 @@ def bubble(point, step, color_bubble):
         sd.circle(center_position=point, radius=radius_bubble, width=2, color=color_bubble)
 
 
-bubble(point=sd.get_point(300, 300), radius_bubble=50, color_bubble=sd.random_color())
-#TODO Вот здесь даёт ошибку, получается что, мы указываем точку то есть point, после указываем
-#TODO радиус то есть radius_bubble, и цвет который будет выдаваться рандомно?
-
+bubble(point=sd.get_point(300, 300), step=5, color_bubble=sd.random_color())
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+for x in range(100, 1001, 100):
+    point_bubble = sd.get_point(x, 100)
+    bubble(point=point_bubble, step=15, color_bubble=sd.COLOR_RED)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        point_bubble = sd.get_point(x, y)
+        bubble(point=point_bubble, step=5, color_bubble=sd.COLOR_WHITE)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+for _ in range(100):
+    point_bubble = sd.random_point()
+    step = random
+    bubble(point=point_bubble, step= 10, color_bubble=sd.random_color())
 
 sd.pause()
 
--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -10,17 +10,22 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
+paper_x, paper_y = 8, 6
 # paper_x, paper_y = 3, 4
 # paper_x, paper_y = 11, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x > paper_x and envelop_y > paper_y:
+    print('Да')
+else:
+    print('Нет')
+
+# TODO В данном случае я понимаю что команда elif не нужна?
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,14 +33,14 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
+brick_x, brick_y, brick_z = 3, 5, 6
 # brick_x, brick_y, brick_z = 3, 6, 5
 # brick_x, brick_y, brick_z = 6, 3, 5
 # brick_x, brick_y, brick_z = 6, 5, 3
@@ -49,4 +54,9 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if hole_x > brick_x and hole_y > brick_y:
+    print('Да')
+else:
+    print('Нет')
+# TODO Только не могу понять как еще проверить brick_z, как будто слишком просто получилось,
+#  не зря же его указали
